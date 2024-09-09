2d1b51d8ce1d0920e348f1742e3e69e81f19b293
Andreyna Carvalho <andreyna.carvalho@codex.com.br>
2024-09-09 20:11:14 -0300

    feat: update try again

--- questions/lib/main.dart
+++ questions/lib/main.dart
@@ -29,6 +29,13 @@ class QuestionsAppState extends State<QuestionApp> {
     }
   }
 
+  void resetQuiz() {
+    setState(() {
+      questionSelected = 0;
+      choices = [];
+    });
+  }
+
   get hasQuestionSelected {
     return questionSelected < questions.length;
   }
@@ -78,6 +85,10 @@ class QuestionsAppState extends State<QuestionApp> {
                     Text(
                       'Sua casa de Hogwarts é: ${result[findMostFrequent(choices)]}',
                       style: const TextStyle(fontSize: 24),
+                    ),
+                    ElevatedButton(
+                      onPressed: resetQuiz,
+                      child: const Text("Tentar novamente"),
                     ),
                   ],
                 ),
